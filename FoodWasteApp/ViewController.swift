e9106fa591fde71d989d3c2720559c8bdcf51b17
Luigi <luigibarretta@outlook.com>
2019-01-31 16:33:31 +0100

    Initial Commit

---
+++ FoodWasteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodWasteApp
+//
+//  Created by Luigi Barretta on 31/01/2019.
+//  Copyright © 2019 UltraVortex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
